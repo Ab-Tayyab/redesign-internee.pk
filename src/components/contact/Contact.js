04adc83ce9d5928494581a8a438d6237159f9a90
abdullahtayyab894 <abdullahtayyab414@gmail.com>
2024-08-24 00:52:15 +0500

    form validation added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,29 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 
 function Contact() {
+  const [error, setError] = useState({});
+  const [toast, setToast] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validate = () => {
+    const newError = {};
+    if (!formData.name) newError.name = "Enter Name";
+
+    if (!formData.email) {
+      newError.email = "Enter Email";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newError.email = "Email address is invalid";
+    }
+
+    if (!formData.phone) newError.phone = "Enter Phone Number";
+
+    if (!formData.message) newError.phone = "Enter Message";
+
+    return newError;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationError = validate();
+    setError(validationError);
+
+    if (Object.keys(validationError).length === 0) {
+      localStorage.setItem("ContactFormData", JSON.stringify(formData));
+
+      const storedData = localStorage.getItem("ContactFormData");
+      console.log("Stored Data:", JSON.parse(storedData));
+      setToast(true);
+    }
+
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    });
+
+    setTimeout(() => {
+      setToast(false);
+    }, 3000);
+  };
   return (
     <div className="contact-container">
       <div className="map-overlay">
         <h1>Project in mind? Let’s Talk 🚀</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Name</label> <br />
-            <input type="text" />
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            {error.name && <p className="error">{error.name}</p>}
           </div>
           <div>
             <label>Email Address</label> <br />
-            <input type="email" />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            {error.email && <p className="error">{error.email}</p>}
           </div>
           <div>
             <label>Phone Number</label> <br />
-            <input type="number" />
+            <input
+              type="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+            {error.phone && <p className="error">{error.phone}</p>}
           </div>
           <div>
             <label>Message</label> <br />
-            <textarea></textarea>
+            <textarea
+              type="text"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+            {error.message && <p className="error">{error.message}</p>}
           </div>
+          <button className="contact-btn" type="submit">
+            Send
+          </button>
+          <p className="contact-instruction">
+            After your message submission, Our team will respond 1-2 Business
+            days
+          </p>
         </form>
+        {toast && <div className="toast">Message sent successfully!</div>}
       </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13614.833837324699!2d74.4173763!3d31.449692150000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1724353863493!5m2!1sen!2s"
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import img from '../../assest/logo.png'
+import img from "../../assest/logo.png";
 import "./Footer.css";
 
 function Footer() {
@@ -22,10 +22,21 @@ function Footer() {
       <div>
         <h1>Socialize with us</h1>
         <div className="footer-icons">
-        <i class="fa fa-instagram" ></i>
-        <i class="fa fa-facebook" ></i>
-        <i class="fa fa-youtube" ></i>
-        <i class="fa fa-whatsapp" ></i>
+          <a href="https://www.youtube.com/@InterneePk" target="_blank">
+            <i class="fa fa-youtube"></i>
+          </a>
+          <a href="https://www.instagram.com/internee.pk/" target="_blank">
+            <i class="fa fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/p/Interneepk-100093222249320/?paipv=0&eav=AfYXIkKRuTl67hdjTOzbcStg5LMe3Vpw1FLiCUaYEuctQy0U3USp0g8HAGJNrzKDvx8&_rdr"
+            target="_blank"
+          >
+            <i class="fa fa-facebook"></i>
+          </a>
+          <a href="https://twitter.com/Internee_pk" target="_blank">
+            <i class="fa fa-twitter"></i>
+          </a>
         </div>
         <h5>+923453191638</h5>
         <h5>nfo@internee.pk</h5>
